948b8503473ceb59bf0af1b97507beb662ad13a5
Kevin Gabriel Alves de Melo <kevinalvesmelo@gmail.com>
2021-01-21 13:51:09 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-
-		System.out.println();
-		
-		Seller seller = new Seller(21, "Kevin", "kevinalvesmelo@gmail.com", new Date(), 1000.00, obj);
-		System.out.println(seller);
-		
-		System.out.println();
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
