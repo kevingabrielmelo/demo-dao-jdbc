2c8d74f6f0c1f5fed729cc8437a2f08f224df0c4
Kevin Gabriel Alves de Melo <kevinalvesmelo@gmail.com>
2021-01-21 12:07:35 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 
+		System.out.println();
+		
+		Seller seller = new Seller(21, "Kevin", "kevinalvesmelo@gmail.com", new Date(), 1000.00, obj);
+		System.out.println(seller);
 	}
 
 }
